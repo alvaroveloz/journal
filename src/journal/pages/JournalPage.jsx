6353ee9fb031107d14135eaff298dcede7c39bb2
alvaroveloz <alvaro.veloz@gmail.com>
2023-01-24 19:16:07 -0500

    JOURNAL - Added Material UI components

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,5 +1,13 @@
+import { Typography } from '@mui/material'
+import { JournalLayout } from '../layout/JournalLayout'
+
 export const JournalPage = () => {
   return (
-    <div>JournalPage</div>
-  )
+    <JournalLayout>
+      <Typography component='h1'>
+        Nisi eu ad ipsum proident ipsum elit exercitation duis consectetur id
+        ullamco deserunt sunt.
+      </Typography>
+    </JournalLayout>
+  );
 }
