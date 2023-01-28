3f9aba2b5d1a0c5e0691498efe9c26a8754e4f82
alvaroveloz <alvaro.veloz@gmail.com>
2023-01-27 20:08:06 -0500

    JOURNAL - Google Sign In Login Page

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,4 +1,5 @@
-import { useDispatch } from 'react-redux';
+import { useMemo } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { Link as RouterLink } from 'react-router-dom';
 import { Button, Grid, Link, TextField, Typography } from '@mui/material';
@@ -10,10 +11,14 @@ import { checkingAuthentication, startGoogleSignIn } from '../../store/auth';
 
 export const LoginPage = () => {
 
+  const { status } = useSelector((state) => state.auth);
+
   const { email, password, onInputChange } = useForm({
     email: '',
     password: '',
   });
+
+  const isAuthenticating = useMemo(() => status === 'checking', [status]);
 
   const dispatch =  useDispatch();
 
@@ -51,19 +56,29 @@ export const LoginPage = () => {
               placeholder='Contraseña'
               fullWidth
               name='password'
-              value={ password }
-              onChange={ onInputChange }
+              value={password}
+              onChange={onInputChange}
             />
           </Grid>
 
           <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
             <Grid item xs={12} sm={6}>
-              <Button variant='contained' fullWidth type='submit'>
+              <Button
+                variant='contained'
+                fullWidth
+                type='submit'
+                disabled={isAuthenticating}
+              >
                 Login
               </Button>
             </Grid>
             <Grid item xs={12} sm={6}>
-              <Button variant='contained' fullWidth onClick={onGoogleSignIn}>
+              <Button
+                variant='contained'
+                fullWidth
+                onClick={onGoogleSignIn}
+                disabled={isAuthenticating}
+              >
                 <Google />
                 <Typography sx={{ ml: 1 }}>Google</Typography>
               </Button>
