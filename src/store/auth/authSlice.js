5322637249cbbe4cfd40dce2bec602c0372928b0
alvaroveloz <alvaro.veloz@gmail.com>
2023-01-27 20:06:49 -0500

    JOURNAL - Added Google sign in

--- src/store/auth/authSlice.js
+++ src/store/auth/authSlice.js
@@ -11,10 +11,22 @@ export const authSlice = createSlice({
     errorMessage: null,
   },
   reducers: {
-    login: (state, action) => {
-      state.value += 1;
+    login: (state, { payload } ) => {
+      state.status = 'authenticated';
+      state.uid = payload.uid;
+      state.email = payload.email;
+      state.displayName = payload.displayName;
+      state.photoURL = payload.photoURL;
+      state.errorMessage = payload.errorMessage;
     },
-    logout: (state, payload) => {},
+    logout: (state,  { payload } ) => {
+      state.status = 'not-authenticated';
+      state.uid = null;
+      state.email = null;
+      state.displayName = null;
+      state.photoURL = null;
+      state.errorMessage = payload.errorMessage;
+    },
     checkingCredentials: (state) => {
         state.status = 'checking';
     },
