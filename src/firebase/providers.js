f6b4bc1732f03f1d52e41d6021d480ff66ae5a97
alvaroveloz <alvaro.veloz@gmail.com>
2023-01-29 14:39:18 -0500

    JOURNAL - Added register, login and logout

--- src/firebase/providers.js
+++ src/firebase/providers.js
@@ -1,4 +1,5 @@
-import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
+import { async } from '@firebase/util';
+import { createUserWithEmailAndPassword, GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup, updateProfile } from 'firebase/auth'
 import { FirebaseAuth } from './config';
 
 const googleProvider = new GoogleAuthProvider();
@@ -27,4 +28,59 @@ export const signInWithGoogle = async() => {
         }
     }
 
+}
+
+
+export const registerUserWithEmailPassword = async ( { email,  password,  displayName} ) => {
+  try {
+
+    console.log(email, password, displayName);
+    const resp = await createUserWithEmailAndPassword(FirebaseAuth, email, password);
+    const { uid, photoURL } = resp.user;
+    
+    //Todo: update displayName in Firebase
+    //After authenticated you are able to call FirebaseAuth.currentUser
+    await updateProfile(FirebaseAuth.currentUser, { displayName });
+
+    return {
+      ok: true,
+      uid, photoURL, email, displayName
+    }
+
+  } catch (error) {
+    return {
+      ok: false,
+      errorMessage: error.message,
+    };
+  }
+};
+
+export const loginWithEmailPassword = async( { email, password } ) => {
+
+  try {
+    
+    const resp = await signInWithEmailAndPassword( FirebaseAuth, email, password );
+    const { uid, photoURL, errorMessage, displayName } = resp.user;
+
+    return {
+      ok: true,
+      uid,
+      photoURL,
+      email,
+      errorMessage,
+      displayName,
+    };
+
+  } catch (error) {
+    return {
+      ok: false,
+      errorMessage: error.message,
+    };
+  }
+
+}
+
+
+export const logoutFirebase = async() => {
+  return await FirebaseAuth.signOut();
 }
